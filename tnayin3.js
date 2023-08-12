4a35266c93ab71f48518170d869102bf4bd70d4d
Enry Vardanyan <Vardanyan.enry7@gmail.com>
2023-08-12 05:09:09 -0700

    Tnayin

---
+++ tnayin3.js
@@ -0,0 +1,15 @@
+let matrix = [[5,6],[10,23],[11,15]]
+function biggestSum(mat){ 
+    let returnedArr = []
+        for(arr of mat){
+            let sum = 0
+            for(el of arr){
+                sum += el
+            }
+            returnedArr.push(sum)
+        }
+ return returnedArr
+}
+console.log(biggestSum(matrix))
+
+
